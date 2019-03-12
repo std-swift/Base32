854ac45e2e30f15782e041311a0510846a42b562
David Skrundz <david@skrundz.ca>
2019-03-11 22:22:15 -0600

    Update the Base32 struct

--- Sources/Base32/Base32.swift
+++ Sources/Base32/Base32.swift
@@ -5,14 +5,14 @@
 
 public struct Base32 {
 	@inlinable
-	public static func decode(_ data: String) -> [UInt8] {
+	public static func decode<T: Sequence>(_ data: T) -> Base32Decoder.Decoded where T.Element == Base32Decoder.Element{
 		var decoder = Base32Decoder()
 		decoder.decode(data)
 		return decoder.finalize()
 	}
 	
 	@inlinable
-	public static func encode<T: Sequence>(_ data: T) -> String where T.Element == UInt8 {
+	public static func encode<T: Sequence>(_ data: T) -> Base32Encoder.Encoded where T.Element == Base32Encoder.Element {
 		var decoder = Base32Encoder()
 		decoder.encode(data)
 		return decoder.finalize()
